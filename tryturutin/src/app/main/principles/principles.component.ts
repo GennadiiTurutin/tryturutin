c3de31686732cc83499c4a4b42f14c2c6d78b4c1
GennadiiTurutin <gennadii.turutin@gmail.com>
2019-03-08 23:50:21 -0500

    Working on style

--- tryturutin/src/app/main/principles/principles.component.ts
+++ tryturutin/src/app/main/principles/principles.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principles.component.scss']
 })
 export class PrinciplesComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit() {
